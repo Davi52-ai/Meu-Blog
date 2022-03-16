844c330df762c3a060a5b2e1389b7f8552dd1a41
Davi52-ai <davigedoz@gmail.com>
2022-03-15 21:23:50 -0300

    criando generetors, model address street number cep uf city

---
+++ db/migrate/20220316001610_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.integer :number
+      t.string :cep
+      t.string :uf
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_200242) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_16_001610) do
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "number"
+    t.string "cep"
+    t.string "uf"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
