1223a288baed81db5e4aacbe6a1080ed25a3813b
Davi52-ai <davigedoz@gmail.com>
2022-03-17 15:41:25 -0300

    string complement addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_16_001610) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_17_183556) do
   create_table "addresses", force: :cascade do |t|
     t.string "street"
     t.string "number"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_16_001610) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "complement"
   end
 
   create_table "categories", force: :cascade do |t|
